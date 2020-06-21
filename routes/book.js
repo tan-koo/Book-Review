75f09321a9f8664a009693fd560e8fd28eaabac3
usergithub-1 <Myname-25@hotmail.com>
2020-06-21 16:23:02 +0700

    update

--- routes/book.js
+++ routes/book.js
@@ -6,6 +6,28 @@ const catalog = require('../model/catagorie')
 // var user  = require('../model/user');
 const comments = require('../model/comment');
 const middleware = require('../middleware');
+const multer = require('multer');
+const path = require('path');
+function escapeRegex(text) {
+  return text.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, "\\$&");
+};
+
+const storage = multer.diskStorage({
+  destination: './public/uploads',
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+})
+
+const imagefilter = function (req, res, cb) {
+  var ext = path.extname(file.originalname);
+  if (ext !== '.png' && ext !== '.gif' && ext !== '.jpg' && ext !== '.jpeg') {
+    return cb(new Error("Type File not include"), false)
+  }
+  cb(null, true);
+}
+
+const upload = multer({ storage: storage, filefilter: imagefilter });
 
 router.get("/", function (req, res) {
   books.find({}, function (error, ALLData) {
@@ -13,61 +35,46 @@ router.get("/", function (req, res) {
       console.log(error)
     }
     else {
-      res.render("landing", { SongMa: ALLData })
+      catalog.find({}, function (err, Allcate) {
+        if (err) {
+          console.log(err)
+        }
+        else {
+          res.render("landing", { SongMa: ALLData, Songma3: Allcate });
+        }
+      })
     }
   })
 });
 
-router.get("/search", async function (req, res) {
-  let key = req.query.keyword;
-  const result = await books.find({ name: { $regex: key } });
-  console.log(result);
-  res.render("search", { ItemSearch: result, key: key });
+router.get("/search", function (req, res) {
+  var noMatch = null;
+  if (req.query.keyword) {
+    const regex = new RegExp(escapeRegex(req.query.keyword), 'gi');
+    // Get all posts from DB
+    books.find({ name: regex }, function (err, allposts) {
+      if (err) {
+        console.log(err);
+      }
+      else {
+        if (allposts.length < 1) {
+          noMatch = "Can not find the post you are looking for";
+        }
+        res.render("search", { ItemSearch: allposts, noMatch: noMatch })
+      }
+    });
+  }
+  else {
+    // Get all posts from DB
+    post.find({}, function (err, allposts) {
+      if (err) {
+        console.log(err);
+      } else {
+        res.render("search", { ItemSearch: allposts, page: 'posts', noMatch: noMatch });
+      }
+    });
+  }
 });
-
-router.get("/niyay", function (req, res) {
-  books.find({ category: "Novel" }, function (error, ALLData2) {
-    if (error) {
-      console.log(error)
-    }
-    else {
-      res.render("showcatagory", { SongMa2: ALLData2 })
-    }
-  })
-})
-
-router.get("/warasan", function (req, res) {
-  books.find({ category: "Magazine" }, function (error, ALLData2) {
-    if (error) {
-      console.log(error)
-    }
-    else {
-      res.render("showcatagory", { SongMa2: ALLData2 })
-    }
-  })
-})
-
-router.get("/cartoon", function (req, res) {
-  books.find({ category: "Cartoon" }, function (error, ALLData2) {
-    if (error) {
-      console.log(error)
-    }
-    else {
-      res.render("showcatagory", { SongMa2: ALLData2 })
-    }
-  })
-})
-
-router.get("/home", function (req, res) {
-  books.find({ category: "House" }, function (error, ALLData2) {
-    if (error) {
-      console.log(error)
-    }
-    else {
-      res.render("showcatagory", { SongMa2: ALLData2 })
-    }
-  })
-})
 
 router.get("/new", function (req, res) {
   res.render("addnewbook");
@@ -77,7 +84,7 @@ router.get("/new", function (req, res) {
 router.post("/", function (req, res) {
   let n_name = req.body.name;
   let n_writer = req.body.writer;
-  let n_imgurl = req.body.imgurl;
+  let n_imgurl = req.file.filename;
   let n_desc = req.body.desc;
   let n_tag = req.body.tag;
   let n_author = {
@@ -94,25 +101,28 @@ router.post("/", function (req, res) {
     }
     else {
       console.log(newdata);
-      // comments.create(
-      //   {
-      //     text: "I am",
-      //     userment:"ToB"
-      //   },function(err, comments){
-      //     if(err){
-      //       console.log("error FUnction Comment")
-      //     }
-      //     else{
-      //       books.comments.push(comments);
-      //       books.save();
-      //       console.log("comment added");
-      //     }
-      //   }
-      // )
       res.redirect("/book");
     }
   })
 });
+
+router.get("/categorie/:text", function (req, res) {
+  books.find({ category: req.params.text }, function (error, AllData2) {
+    if (error) {
+      console.log("error");
+    }
+    else {
+      catalog.find({}, function (err, Allcate) {
+        if (err) {
+          console.log("error")
+        }
+        else {
+          res.render("showcatagory", { SongMa2: AllData2, SongMa3: Allcate })
+        }
+      })
+    }
+  })
+})
 
 // detail
 router.get("/:id", middleware.loginyoung, function (req, res) {
@@ -141,7 +151,7 @@ router.get("/:id/edit", middleware.chechbookOwnership, function (req, res) {
 router.put("/:id", middleware.chechbookOwnership, function (req, res) {
   let n_name = req.body.name;
   let n_writer = req.body.writer;
-  let n_img = req.body.imgurl;
+  let n_img = req.file.filename;
   let n_desc = req.body.desc;
   let n_tag = req.body.tag;
   var n_card = { name: n_name, writer: n_writer, imgurl: n_img, desc: n_desc, category: n_tag };
